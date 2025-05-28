64fac800caf880618c9df5b550ad7efc0873eca1
jafar fuad <jata1518@gmail.com>
2025-05-28 23:54:44 +0300

    Add Reservation entity with key properties to CarRentalApp.Core.Entities

---
+++ CarRentalApp.Core/Entities/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Core.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
