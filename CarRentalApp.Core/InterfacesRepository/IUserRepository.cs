80e594620c970817becaa9c1fcf7fd5d1712014c
jafar fuad <jata1518@gmail.com>
2025-05-29 00:48:13 +0300

    Add IUserRepository interface for user-related data operations

---
+++ CarRentalApp.Core/InterfacesRepository/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using CarRentalApp.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Core.InterfacesRepository
+{
+    interface IUserRepository
+    {
+        Task<List<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task  AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+    }
+}
