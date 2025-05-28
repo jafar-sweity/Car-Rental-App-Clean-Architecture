0f8d4ef5dbbda7694e321782cdbd3a9538b9259b
jafar fuad <jata1518@gmail.com>
2025-05-28 23:59:57 +0300

    Add ApplicationDbContext with DbSets for User, Car, and Reservation entities

---
+++ CarRentalApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CarRentalApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Infrastructure.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }  
+    }
+}
