f2b3e3bf1a535f3df0fa3fb4b4bd4cec2ee6108f
jafar fuad <jata1518@gmail.com>
2025-05-29 00:49:25 +0300

    Add ICarRepository interface for car-related data operations

---
+++ CarRentalApp.Core/InterfacesRepository/ICarRepository.cs
@@ -0,0 +1,18 @@
+﻿using CarRentalApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Core.InterfacesRepository
+{
+    interface ICarRepository
+    {
+        Task<List<Car>> GetAllCarsAsync();
+        Task<Car> GetCarByIdAsync(int id);
+        Task AddCarAsync(Car car);
+        Task UpdateCarAsync(Car car);
+        Task DeleteCarAsync(int id);
+    }
+}
