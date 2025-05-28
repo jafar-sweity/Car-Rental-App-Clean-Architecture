203f230e22cff37b08bef8a3c50cc92d0628eb8b
jafar fuad <jata1518@gmail.com>
2025-05-29 00:26:51 +0300

    Add connection string and migration for InitialCreate

--- CarRentalApp.Infrastructure/Data/ApplicationDbContext.cs
+++ CarRentalApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CarRentalApp.Infrastructure.Data
 {
-    class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- CarRentalApp.Web/Program.cs
+++ CarRentalApp.Web/Program.cs
@@ -1,15 +1,22 @@
+using CarRentalApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Corrected the AddDbContext method usage
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
