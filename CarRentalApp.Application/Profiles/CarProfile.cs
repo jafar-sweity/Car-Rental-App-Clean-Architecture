3e07b5c8eb442f5b4689e8871b133c7df9919af3
jafar fuad <jata1518@gmail.com>
2025-05-29 15:18:49 +0300

    Add AutoMapper profile for Car entity and DTO mappings

---
+++ CarRentalApp.Application/Profiles/CarProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CarRentalApp.Application.Models.Car;
+using CarRentalApp.Core.Entities;
+
+
+namespace CarRentalApp.Application.Profiles
+{
+    class CarProfile : Profile
+    {
+        public CarProfile()
+        {
+            CreateMap<Car, CarDto>().ReverseMap();
+            CreateMap<CarCreateDto, Car>();
+            CreateMap<CarUpdateDto, Car>().ReverseMap();
+        }
+    }
+}
