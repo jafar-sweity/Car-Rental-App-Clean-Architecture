b25cb338313fa1965329381c67cb770581cb246f
jafar fuad <jata1518@gmail.com>
2025-05-29 15:09:42 +0300

    Add UserDto, UserCreateDto, and UserUpdateDto models for user data transfer

---
+++ CarRentalApp.Application/Models/User/UserCreateDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Application.Models.User
+{
+    public record UserCreateDto
+    (
+           string FirstName,
+           string LastName,
+           string Email,
+           string PhoneNumber,
+           DateTime? DateOfBirth,
+           string AddressLine1,
+           string AddressLine2,
+           string City,
+           string Country,
+           string DriversLicenseNumber
+    );
+}
