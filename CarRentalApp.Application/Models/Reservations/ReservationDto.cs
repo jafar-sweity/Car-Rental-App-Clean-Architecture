4f69da664a93fcfbed026092dc6f4acc376e4363
jafar fuad <jata1518@gmail.com>
2025-05-29 15:12:12 +0300

    Add ReservationDto, ReservationCreateDto, and ReservationUpdateDto models for reservation data transfer

---
+++ CarRentalApp.Application/Models/Reservations/ReservationDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Application.Models.Reservations
+{
+    public record ReservationDto
+    (
+        int Id,
+        int CarId,
+        int UserId,
+        DateTime StartDate,
+        DateTime EndDate,
+        decimal TotalCost
+    );
+}
---
+++ CarRentalApp.Application/Models/Reservations/ReservationUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Application.Models.Reservations
+{
+    public record ReservationUpdateDto
+    (
+      int CarId,
+      int UserId,
+      DateTime StartDate,
+      DateTime EndDate,
+      decimal TotalCost
+    );
+}
