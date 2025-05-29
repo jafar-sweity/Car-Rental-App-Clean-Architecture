15a2e8c8848f22f3331cca29dc390f87881fb7af
jafar fuad <jata1518@gmail.com>
2025-05-29 15:22:18 +0300

    Add AutoMapper profile for Reservation entity and DTO mappings

---
+++ CarRentalApp.Application/Profiles/ReservationProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CarRentalApp.Application.Models.Reservations;
+using CarRentalApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalApp.Application.Profiles
+{
+    class ReservationProfile : Profile
+    {
+        public ReservationProfile()
+        {
+            CreateMap<Reservation, ReservationDto>().ReverseMap();
+            CreateMap<ReservationCreateDto, Reservation>();
+            CreateMap<ReservationUpdateDto, Reservation>().ReverseMap();
+        }
+    }
+}
